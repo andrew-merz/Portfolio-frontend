5d1f1a2977b5efcc4757c76a5f5ecf2eaa394915
andrew-merz <merz.school@gmail.com>
2022-05-11 15:44:55 -0400

    got rid of errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import "../index.css";
 function Header(props) {
   //inline style for the nav tag
   const navStyle = {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
@@ -7,11 +7,11 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // wrap our application inside of router to enable using router
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
